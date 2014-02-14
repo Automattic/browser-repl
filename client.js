a60b7418555c9141dee199fa8372a982c4fb35d4
Guillermo Rauch <rauchg@gmail.com>
2014-02-14 11:13:24 -0800

    improve browserify build

--- client.js
+++ client.js
@@ -1,16 +1,16 @@
-var util = require('util');
+var io = require('socket.io-client');
+var inspect = require('util-inspect');
 
 var socket = io();
 socket.on('run', function(js, fn){
   try {
     var rtn = eval(js);
-    fn(null, util.inspect(rtn, {
-      colors: true
-    }));
+    fn(null, inspect(rtn, { colors: true }));
   } catch(e) {
     fn(e.stack || e.message);
   }
 });
+
 window.onerror = function(err){
   if (!err) err = 'Unknown global error on `window`.';
   if (err.stack) err = err.stack;
